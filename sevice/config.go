757e72676644eeeea49ec81fb15943427096d749
Symvago <45039243+Symvago@users.noreply.github.com>
2018-11-14 23:52:11 +0800

    Update config.go

--- sevice/config.go
+++ sevice/config.go
@@ -14,3 +14,13 @@ func Init() {
 	DetResultSer = model.GetDetectResultHandler()
 	fmt.Println("service init success")
 }
+func Log(v ...interface{}) {
+	log.Println(v...)
+}
+
+
+func CheckError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+	}
+}
